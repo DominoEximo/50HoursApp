f9de8b849e1183fb6696000ab584ccc9b0b9eb09
PappDávid <PappDavid32@gmail.com>
2024-01-29 15:31:53 +0100

    initial commit

---
+++ src/main/java/hu/inf/unideb/thesis/controller/HomeController.java
@@ -0,0 +1,16 @@
+package hu.inf.unideb.thesis.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "That's pretty basic!";
+    }
+
+}
