41149080e997dcf030a97b2dc70a9d866e14d91c
PappDávid <PappDavid32@gmail.com>
2024-02-04 12:17:29 +0100

    reformatted user registration

--- src/main/java/hu/inf/unideb/thesis/controller/UserController.java
+++ src/main/java/hu/inf/unideb/thesis/controller/UserController.java
@@ -1,8 +1,11 @@
 package hu.inf.unideb.thesis.controller;
 
 import hu.inf.unideb.thesis.entity.User;
+import hu.inf.unideb.thesis.service.RoleService;
 import hu.inf.unideb.thesis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -16,15 +19,19 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RoleService roleService;
+
     @PostMapping(value = "/signup")
-    public String registerUser(@RequestBody User user){
+    public ResponseEntity<String> registerUser(@RequestBody User user){
 
         if(userService.findByName(user.getName()) != null){
             throw new RuntimeException("User already exists");
         }
         else {
+            user.setRoles(List.of(roleService.findByName("USER")));
             userService.save(user);
-            return user.getName() + "registered";
+            return new ResponseEntity<>(user.getName() +" " + "registered!", HttpStatus.OK);
         }
     }
 
